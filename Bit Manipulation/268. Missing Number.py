a7e2803f163542083ef4add925e3dcea482cc141
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-07-22 14:38:34 -0400

    Create 268. Missing Number.py

---
+++ Bit Manipulation/268. Missing Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        while i < n: 
+            if nums[i]<n and nums[i]!=i: 
+                s = nums[i]
+                nums[i], nums[s] = nums[s], nums[i]
+            else:
+                i += 1 
+        for i in range(n):
+            if nums[i] != i: 
+                return i
+        return n
