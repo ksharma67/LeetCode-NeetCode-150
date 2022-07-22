4a68fa77f3604ae606db450c0f438ef750bba5e5
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-07-22 18:40:49 -0400

    Create 39. Combination Sum.py

---
+++ Backtracking/39. Combination Sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                return
+
+            for i in range(start,len(candidates)):
+                comb.append(candidates[i])
+                backtrack(remain - candidates[i], comb, i)
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
