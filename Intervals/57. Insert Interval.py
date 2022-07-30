b561afe89a4fbd79128f8bcb8d5ca2d865c37328
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-07-30 13:20:17 -0400

    Create 57. Insert Interval.py

---
+++ Intervals/57. Insert Interval.py
@@ -0,0 +1,32 @@
+class Solution:
+    def insert(self, intervals: 'List[Interval]', newInterval: 'Interval') -> 'List[Interval]':
+        # init data
+        new_start, new_end = newInterval
+        idx, n = 0, len(intervals)
+        output = []
+        
+        # add all intervals starting before newInterval
+        while idx < n and new_start > intervals[idx][0]:
+            output.append(intervals[idx])
+            idx += 1
+            
+        # add newInterval
+        # if there is no overlap, just add the interval
+        if not output or output[-1][1] < new_start:
+            output.append(newInterval)
+        # if there is an overlap, merge with the last interval
+        else:
+            output[-1][1] = max(output[-1][1], new_end)
+        
+        # add next intervals, merge with newInterval if needed
+        while idx < n:
+            interval = intervals[idx]
+            start, end = interval
+            idx += 1
+            # if there is no overlap, just add an interval
+            if output[-1][1] < start:
+                output.append(interval)
+            # if there is an overlap, merge with the last interval
+            else:
+                output[-1][1] = max(output[-1][1], end)
+        return output
