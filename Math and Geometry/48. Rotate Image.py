198d987c4c0d084e80bfa971c77ce5f0fb3f9166
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-08-02 14:04:10 -0400

    Create 48. Rotate Image.py

---
+++ Math and Geometry/48. Rotate Image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
