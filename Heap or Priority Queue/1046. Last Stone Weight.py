a7aed5492b86e536bba4e9481bccfb01306b35d0
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-08-19 21:13:21 -0400

    Create 1046. Last Stone Weight.py

---
+++ Heap or Priority Queue/1046. Last Stone Weight.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        for i in range(len(stones)):
+            stones[i] *= -1
+
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            stone_1 = heapq.heappop(stones)
+            stone_2 = heapq.heappop(stones)
+            if stone_1 != stone_2:
+                heapq.heappush(stones, stone_1 - stone_2)
+
+        return -heapq.heappop(stones) if stones else 0
