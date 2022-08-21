248a6d6024522c89af75aaa91d8b6456731f3da2
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-08-21 10:51:45 -0400

    Create 102. Binary Tree Level Order Traversal.py

---
+++ Trees/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,22 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+        
+        def helper(node, level):
+            # start the current level
+            if len(levels) == level:
+                levels.append([])
+
+            # append the current node value
+            levels[level].append(node.val)
+
+            # process child nodes for the next level
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+            
+        helper(root, 0)
+        return levels
