6e188cd40bfaa90be386aace46e59588aabe8f11
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-08-22 12:10:53 -0400

    Create 297. Serialize and Deserialize Binary Tree.py

---
+++ Trees/297. Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,23 @@
+# Deserialization 
+class Codec:
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        :type data: str
+        :rtype: TreeNode
+        """
+        def rdeserialize(l):
+            """ a recursive helper function for deserialization."""
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+                
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
