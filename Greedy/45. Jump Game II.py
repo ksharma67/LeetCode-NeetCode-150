4b5c8bb392aa467caed91143ceb57ced6c799956
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-09-05 10:28:49 -0400

    Create 45. Jump Game II.py

---
+++ Greedy/45. Jump Game II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+            jumps = 0
+            current_jump_end = 0
+            farthest = 0
+            for i in range(len(nums) - 1):
+                # we continuously find the how far we can reach in the current jump
+                farthest = max(farthest, i + nums[i])
+                # if we have come to the end of the current jump,
+                # we need to make another jump
+                if i == current_jump_end:
+                    jumps += 1
+                    current_jump_end = farthest
+            return jumps
