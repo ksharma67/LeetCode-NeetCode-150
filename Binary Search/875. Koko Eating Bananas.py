2a4b4e80f776ea0e70ba44eb67f7ca519442c40c
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-10-08 17:00:11 -0400

    Create 875. Koko Eating Bananas.py

---
+++ Binary Search/875. Koko Eating Bananas.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:  
+        # Initalize the left and right boundaries     
+        left = 1
+        right = max(piles)
+        
+        while left < right:
+            # Get the middle index between left and right boundary indexes.
+            # hour_spent stands for the total hour Koko spends.
+            middle = (left + right) // 2            
+            hour_spent = 0
+            
+            # Iterate over the piles and calculate hour_spent.
+            # We increase the hour_spent by ceil(pile / middle)
+            for pile in piles:
+                hour_spent += math.ceil(pile / middle)
+            
+            # Check if middle is a workable speed, and cut the search space by half.
+            if hour_spent <= h:
+                right = middle
+            else:
+                left = middle + 1
+        
+        # Once the left and right boundaries coincide, we find the target value,
+        # that is, the minimum workable eating speed.
+        return right
