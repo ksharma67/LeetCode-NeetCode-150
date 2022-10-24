30a07f8a0587eb063e6afe9c01f9b6e5850f48e9
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-10-24 12:32:37 -0400

    Update 312. Burst Balloons.py

--- 2-D Dynamic Programming/312. Burst Balloons.py
+++ 2-D Dynamic Programming/312. Burst Balloons.py
@@ -6,23 +6,23 @@ class Solution:
 
         # handle edge case
         nums = [1] + nums + [1]
+        n = len(nums)
+        # dp[i][j] represents
+        # maximum if we burst all nums[left]...nums[right], inclusive
+        dp = [[0] * n for _ in range(n)]
 
-        @lru_cache(None)  # memoization
-        def dp(left, right):
-            # maximum if we burst all nums[left]...nums[right], inclusive
-            if right - left < 0:
-                return 0
-            result = 0
-            # find the last burst one in nums[left]...nums[right]
-            for i in range(left, right + 1):
-                # nums[i] is the last burst one
-                gain = nums[left - 1] * nums[i] * nums[right + 1]
-                # nums[i] is fixed, recursively call left side and right side
-                remaining = dp(left, i - 1) + dp(i + 1, right)
-                # update the result
-                result = max(result, remaining + gain)
-            return result
-
-        # we can not burst the first one and the last one
-        # since they are both fake balloons added by ourselves
-        return dp(1, len(nums) - 2)
+        # do not include the first one and the last one
+        # since they are both fake balloons added by ourselves and we can not
+        # burst them
+        for left in range(n - 2, 0, -1):
+            for right in range(left, n - 1):
+                # find the last burst one in nums[left]...nums[right]
+                for i in range(left, right + 1):
+                    # nums[i] is the last burst one
+                    gain = nums[left - 1] * nums[i] * nums[right + 1]
+                    # recursively call left side and right side
+                    remaining = dp[left][i - 1] + dp[i + 1][right]
+                    # update
+                    dp[left][right] = max(remaining + gain, dp[left][right])
+        # burst nums[1]...nums[n-2], excluding the first one and the last one
+        return dp[1][n - 2]
